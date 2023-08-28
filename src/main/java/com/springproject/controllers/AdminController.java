c79cb3d268a2587b3a50fb3457b1f6314ab1f1d8
RiderTo4ka <132078359+RiderTo4ka@users.noreply.github.com>
2023-08-28 17:46:19 +0300

    O sorrow.

--- src/main/java/com/springproject/controllers/AdminController.java
+++ src/main/java/com/springproject/controllers/AdminController.java
@@ -4,47 +4,45 @@ import com.springproject.models.User;
 import com.springproject.service.RoleService;
 import com.springproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
-@RequestMapping(value = "/admin")
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
 public class AdminController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
+    private final RoleService roleService;
 
     @Autowired
-    private RoleService roleService;
-
-
-    @GetMapping
-    public String adminWelcome(Model model) {
-        model.addAttribute("userList", userService.getUsers());
-        model.addAttribute("roleList",roleService.getRoles());
-        model.addAttribute("user", new User());
-        return "admin";
+    public AdminController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
     }
+
+
+    @GetMapping("/getUsers")
+    public List<User> admin() {
+        return userService.getUsers();
+    }
+
 
     @PostMapping("/saveUser")
     public String saveUser(@ModelAttribute User user) {
         userService.saveUser(user);
-        return "redirect:/admin";
+        return "{\"status\":\"success\"}";
     }
 
     @GetMapping("/edit/{id}")
-    public String editUser(@PathVariable("id") Long id, Model model) {
-        User user = userService.getUserById(id);
-        model.addAttribute("user",user);
-        model.addAttribute("roleList",roleService.getRoles());
-        return "edit";
+    public User editUser(@RequestParam Long id) {
+        return userService.getUserById(id);
     }
-
-
-    @PostMapping("/deleteUser/{id}")
-    public String deleteUser(@PathVariable("id") Long id){
-        userService.deleteUser(id);
-        return "redirect:/admin";
-    }
+//
+//
+//    @PostMapping("/deleteUser/{id}")
+//    public void deleteUser(@PathVariable("id") Long id){
+//        userService.deleteUser(id);
+//    }
 }
---
+++ src/main/java/com/springproject/controllers/ViewController.java
@@ -0,0 +1,41 @@
+package com.springproject.controllers;
+
+import com.springproject.models.User;
+import com.springproject.service.RoleService;
+import com.springproject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public ViewController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/admin")
+    public String adminView(Model model){
+        model.addAttribute("userList", userService.getUsers());
+        model.addAttribute("roleList",roleService.getRoles());
+        model.addAttribute("user", new User());
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String userVies(Model model){
+
+
+
+        return "user";
+    }
+
+
+
+}
