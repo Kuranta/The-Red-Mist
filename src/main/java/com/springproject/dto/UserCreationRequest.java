fbfe17f14f51f08f7f6cbf483cbfa40114efb75d
RiderTo4ka <132078359+RiderTo4ka@users.noreply.github.com>
2023-12-09 11:21:07 +0300

    Minor bugfixes, v.1.2.1

--- src/main/java/com/springproject/dto/UserCreationRequest.java
+++ src/main/java/com/springproject/dto/UserCreationRequest.java
@@ -1,9 +1,12 @@
 package com.springproject.dto;
 
 import com.springproject.models.Role;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.Set;
 
+@NotEmpty
 public record UserCreationRequest (String firstName, String lastName, Integer age, String email, String password, Set<Role> roles){
 }
--- src/main/java/com/springproject/models/User.java
+++ src/main/java/com/springproject/models/User.java
@@ -1,10 +1,7 @@
 package com.springproject.models;
 
-import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
